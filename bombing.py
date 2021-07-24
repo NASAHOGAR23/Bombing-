c10dbbe4a5173d918a1afebee1726675955aaa6f
NASA HOGAR <87891042+NASAHOGAR23@users.noreply.github.com>
2021-07-24 13:47:37 +0600

    Update bombing.py

--- bombing.py
+++ bombing.py
@@ -19,7 +19,7 @@ print(""""
 
 number=str(input("Enter The Number(without:+880):")
 
-amount=int(input("Enter The Amount:"))
+amount=int(input("Enter The Amount:")
 
 api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
 
